81ad886e5b0406714b5abda78d55631b2f2fbb77
Andrew Johnson <drewej@protonmail.com>
2020-04-05 13:02:41 -0400

    Add pure cython to comparison; minor cleanups

--- mcpi.py
+++ mcpi.py
@@ -18,6 +18,13 @@ except ImportError:
         "Unable to import fortran module. Have you built it yet?"
     )
 
+try:
+    from purecython import mcpi as calc_pi_purecython
+except ImportError:
+    raise ImportError(
+        "Unable to import purecython module. Have you build it yet?"
+    )
+
 
 def timed(f):
     """Decorator for timing function calls from python"""
@@ -33,7 +40,7 @@ def timed(f):
 @timed
 def mcpy(n):
     s = 0
-    for i in range(n):
+    for _i in range(n):
         x = random()
         y = random()
         if sqrt(x * x + y * y) <= 1:
@@ -46,13 +53,20 @@ def mcpi_f90(n):
     return calc_pi_f90(n)
 
 
+@timed
+def mcpi_purecython(n):
+    return calc_pi_purecython(n)
+
+
 def compare_pi(samples):
-    bins_py = empty((samples.size, 2), 'd')
+    bins_py = empty((samples.size, 2))
     bins_f90 = empty_like(bins_py)
+    bins_pcython = empty_like(bins_py)
     for i, n in enumerate(samples):
         bins_py[i] = mcpy(n)
         bins_f90[i] = mcpi_f90(n)
-    return samples, bins_py.T, bins_f90.T
+        bins_pcython[i] = mcpi_purecython(n)
+    return samples, bins_py.T, bins_f90.T, bins_pcython.T
 
 
 if __name__ == '__main__':
@@ -61,20 +75,24 @@ if __name__ == '__main__':
     samples = array([
         100, 500, 1000, 5000, 10000, 20000, 30000, 40000, 50000
     ]).repeat(3)
-    s, bp, bf = compare_pi(samples)
+    labels = ["python", "fortran", "cython"]
+    results = compare_pi(samples)
     pyplot.figure()
-    pyplot.semilogx(s, bp[0], 'o', label='python')
-    pyplot.semilogx(s, bf[0], 'x', label='fortran')
+    for label, value in zip(labels, results[1:]):
+        pyplot.scatter(results[0], value[0], label=label)
     pyplot.xlabel("Sampled points")
+    pyplot.xscale("log")
     pyplot.ylabel("Estimated $\\pi$")
-    pyplot.axhline(pi, c='r')
+    pyplot.axhline(pi, c='tab:red', alpha=0.5)
     pyplot.legend()
 
     pyplot.figure()
-    pyplot.loglog(s, bp[1] * 1000, 'o', label='python')
-    pyplot.loglog(s, bf[1] * 1000, 'x', label='fortran')
+    for label, value in zip(labels, results[1:]):
+        pyplot.scatter(results[0], value[1], label=label)
     pyplot.xlabel("Sampled points")
     pyplot.ylabel("Run time [ms]")
+    pyplot.xscale("log")
+    pyplot.yscale("log")
     pyplot.legend()
 
     pyplot.figure()
